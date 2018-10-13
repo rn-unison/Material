90b018b97fcd7c1b54ec1a2254434d703f48e182
juliowaissman <juliowaissman@gmail.com>
2018-10-12 21:15:34 -0700

    a ver ahora

---
+++ presentaciones/rcnn/auto/rcnn_intro.el
@@ -0,0 +1,53 @@
+(TeX-add-style-hook
+ "rcnn_intro"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "presentation" "t")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("babel" "spanish") ("bm" "spanish")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "listings"
+    "xcolor"
+    "babel"
+    "bm")
+   (LaTeX-add-xcolor-definecolors
+    "mc@lstidentifier"
+    "mc@lstcomment"
+    "mc@lststring"
+    "mc@lstkeyword"
+    "mc@lstbackground"
+    "mc@lstframe"
+    "TeXbackgroundcolor"
+    "TeXrulecolor"
+    "mylinkcolor")
+   (LaTeX-add-listings-lstdefinestyles
+    "TeX"))
+ :latex)
+
